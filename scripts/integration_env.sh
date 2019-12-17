1050df5110990226a9b1fc70dadfebc5433db5ea
Becca Petrin <beccapetrin@gmail.com>
2019-12-17 14:54:44 -0800

    wip edit integration script for local env

--- scripts/integration_env.sh
+++ scripts/integration_env.sh
@@ -6,7 +6,6 @@ else
   base64cmd="base64 -d"
 fi
 
-VAULT_VER=$(curl https://api.github.com/repos/hashicorp/vault/tags?page=1 | python -c "import sys, json; print(json.load(sys.stdin)[0]['name'][1:])")
 VAULT_PORT=8200
 SAMBA_VER=4.8.12
 
@@ -19,43 +18,13 @@ DOMAIN_USER_PASS=gracePa55word!
 
 SAMBA_CONF_FILE=/srv/etc/smb.conf
 DOMAIN_NAME=matrix
-DNS_NAME=matrix.lan
+DNS_NAME=host
 REALM_NAME=MATRIX.LAN
 DOMAIN_DN=DC=MATRIX,DC=LAN
 TESTS_DIR=/tmp/vault_plugin_tests
 
-function start_infrastructure() {
-  create_network
-  start_domain
-  start_vault
-}
-
-function stop_infrastructure() {
-  stop_domain_joined_container
-  stop_vault
-  stop_domain
-  delete_network
-}
-
-function create_network() {
-  docker network create ${DNS_NAME}
-}
-
-function delete_network() {
-  docker network rm ${DNS_NAME}
-}
-
-function start_vault() {
-  VAULT_CONTAINER=$(docker run --net=${DNS_NAME} -d -ti --cap-add=IPC_LOCK -v $(pwd)/pkg/linux_amd64:/plugins:Z -e "VAULT_DEV_ROOT_TOKEN_ID=${VAULT_TOKEN}" -e "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:${VAULT_PORT}" -p ${VAULT_PORT}:${VAULT_PORT} vault:${VAULT_VER} server -dev -dev-plugin-dir=/plugins)
-  export VAULT_ADDR=http://127.0.0.1:${VAULT_PORT}
-}
-
-function stop_vault() {
-  docker rm -f ${VAULT_CONTAINER}
-}
-
 function start_domain() {
-  SAMBA_CONTAINER=$(docker run --net=${DNS_NAME} -d -ti --privileged -p 135:135 -p 137:137 -p 138:138 -p 139:139 -p 389:389 -p 445:445 -p 464:464 -p 636:636 -p 3268:3268 -p 3269:3269 -e "SAMBA_DC_ADMIN_PASSWD=${DOMAIN_ADMIN_PASS}" -e "KERBEROS_PASSWORD=${DOMAIN_ADMIN_PASS}" -e SAMBA_DC_DOMAIN=${DOMAIN_NAME} -e SAMBA_DC_REALM=${REALM_NAME} "bodsch/docker-samba4:${SAMBA_VER}")
+  SAMBA_CONTAINER=$(docker run --net=${DNS_NAME} -d -ti --privileged -e "SAMBA_DC_ADMIN_PASSWD=${DOMAIN_ADMIN_PASS}" -e "KERBEROS_PASSWORD=${DOMAIN_ADMIN_PASS}" -e SAMBA_DC_DOMAIN=${DOMAIN_NAME} -e SAMBA_DC_REALM=${REALM_NAME} "bodsch/docker-samba4:${SAMBA_VER}")
   # shouldn't need to publish all these ports as they are only used within the docker network, but figured it may be useful for debugging
 }
 
@@ -118,18 +87,16 @@ function create_keytab() {
 
 function add_vault_spn() {
   docker exec $SAMBA_CONTAINER \
-    samba-tool spn add HTTP/${VAULT_CONTAINER:0:12} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
+    samba-tool spn add HTTP/localhost ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
   docker exec $SAMBA_CONTAINER \
-    samba-tool spn add HTTP/${VAULT_CONTAINER:0:12}:${VAULT_PORT} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
+    samba-tool spn add HTTP/localhost:${VAULT_PORT} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
   docker exec $SAMBA_CONTAINER \
-    samba-tool spn add HTTP/${VAULT_CONTAINER:0:12}.${DNS_NAME} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
+    samba-tool spn add HTTP/localhost.${DNS_NAME} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
   docker exec $SAMBA_CONTAINER \
-    samba-tool spn add HTTP/${VAULT_CONTAINER:0:12}.${DNS_NAME}:${VAULT_PORT} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
+    samba-tool spn add HTTP/localhost.${DNS_NAME}:${VAULT_PORT} ${DOMAIN_VAULT_ACCOUNT} --configfile=${SAMBA_CONF_FILE}
 }
 
 function enable_plugin() {
-  VAULT_PLUGIN_SHA=$(openssl dgst -sha256 pkg/linux_amd64/vault-plugin-auth-kerberos|cut -d ' ' -f2)
-  vault write sys/plugins/catalog/auth/kerberos sha_256=${VAULT_PLUGIN_SHA} command="vault-plugin-auth-kerberos"
   vault auth enable -passthrough-request-headers=Authorization -allowed-response-headers=www-authenticate kerberos
   vault write auth/kerberos/config keytab=@vault_svc.keytab.base64 service_account="vault_svc"
   vault write auth/kerberos/config/ldap binddn=${DOMAIN_VAULT_ACCOUNT}@${REALM_NAME} bindpass=${DOMAIN_VAULT_PASS} groupattr=sAMAccountName groupdn="${DOMAIN_DN}" groupfilter="(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))" insecure_tls=true starttls=true userdn="CN=Users,${DOMAIN_DN}" userattr=sAMAccountName upndomain=${REALM_NAME} url=ldaps://${SAMBA_CONTAINER:0:12}.${DNS_NAME}
@@ -141,13 +108,13 @@ function write_python_test() {
 import kerberos
 import requests
 
-host = \"${VAULT_CONTAINER:0:12}.${DNS_NAME}:${VAULT_PORT}\"
+host = \"localhost.${DNS_NAME}:${VAULT_PORT}\"
 service = \"HTTP@{}\".format(host)
 rc, vc = kerberos.authGSSClientInit(service=service, mech_oid=kerberos.GSS_MECH_OID_SPNEGO)
 kerberos.authGSSClientStep(vc, \"\")
 kerberos_token = kerberos.authGSSClientResponse(vc)
 
-r = requests.post(\"http://{}/v1/auth/kerberos/login\".format(host),
+r = requests.post(\"http://localhost:8200/v1/auth/kerberos/login\",
                   headers={'Authorization': 'Negotiate ' + kerberos_token})
 print('Vault token through Python:', r.json()['auth']['client_token'])
 " > manual_test.py
@@ -187,51 +154,37 @@ function remove_files() {
   rm -fr $TESTS_DIR/integration # using superfluous child dir in case variable is blank at some point ;)
 }
 
-function start_domain_joined_container() {
-  DOMAIN_JOINED_CONTAINER=$(docker run --net=${DNS_NAME} -d -v "${TESTS_DIR}/integration:/tests:Z" -e KRB5_CONFIG=/tests/krb5.conf -e KRB5_CLIENT_KTNAME=/tests/grace.keytab -t python:3.7 cat)
-}
-
-function stop_domain_joined_container() {
-  docker rm -f ${DOMAIN_JOINED_CONTAINER}
-}
-
 function run_test_script() {
   # execute a login from go and record result
-  docker cp bin/login-kerb $DOMAIN_JOINED_CONTAINER:/usr/local/bin/login-kerb
-  VAULT_CONTAINER_PREFIX=${VAULT_CONTAINER:0:12}
-  docker exec $DOMAIN_JOINED_CONTAINER \
-    login-kerb \
-      -username=$DOMAIN_USER_ACCOUNT \
-      -service="HTTP/$VAULT_CONTAINER_PREFIX.$DNS_NAME:8200" \
-      -realm=$REALM_NAME \
-      -keytab_path="/tests/grace.keytab" \
-      -krb5conf_path="/tests/krb5.conf" \
-      -vault_addr="http://$VAULT_CONTAINER_PREFIX.$DNS_NAME:8200"
+  go run cmd/login-kerb/main.go \
+    -username=$DOMAIN_USER_ACCOUNT \
+    -service="HTTP/localhost:8200" \
+    -realm=$REALM_NAME \
+    -keytab_path="/tests/grace.keytab" \
+    -krb5conf_path="/tests/krb5.conf" \
+    -vault_addr="http://localhost:8200"
   go_login_result=$?
 
   # execute a login from python and record result
-  docker exec $DOMAIN_JOINED_CONTAINER \
-    pip install --quiet requests-kerberos
-  docker exec $DOMAIN_JOINED_CONTAINER \
-    python /tests/manual_test.py
+  pip install --quiet requests-kerberos
+  python $TESTS_DIR/integration/manual_test.py
   python_login_result=$?
 }
 
 function run_tests() {
   remove_files
   prepare_files
-  start_domain_joined_container
   run_test_script
 }
 
 function main() {
-  start_infrastructure
+  start_domain
   sleep 15  # could loop until `ldapsearch` returns properly....
   setup_users
   add_vault_spn
   enable_plugin
   run_tests
-  stop_infrastructure
+  stop_domain
   if [ ! $python_login_result = 0 ]; then
     echo "python login failed"
     return $python_login_result
